2ea1791cfe618d9bc46268833c74f28232582b33
Poyraz Özzengi <66021457+poyrazozzengi@users.noreply.github.com>
2024-09-19 00:07:06 +0300

    Update QuestionAnswerer.swift

--- QuestionBot/QuestionAnswerer.swift
+++ QuestionBot/QuestionAnswerer.swift
@@ -1,6 +1,19 @@
 struct MyQuestionAnswerer {
+    func lowercase(word: String) -> String {
+        return word.lowercased()
+    }
+    
     func responseTo(question: String) -> String {
-        
-        return "Sorry, I dont have any idea 🤷🏿‍♂️. "
+        let lowercasedQuestion = lowercase(word: question)
+       
+        if lowercasedQuestion.hasPrefix("hello") {
+            return "Hey Man!"
+        } else {
+            return "Sorry, I don't have any idea 🤷🏿‍♂️."
+        }
     }
 }
+
+
+
+   
